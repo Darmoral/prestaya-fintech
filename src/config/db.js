2eec74daad7d2793554c2df739786fcbd7916c45
Darma <darmabel.orozco@gmail.com>
2026-04-12 15:16:34 -0500

    fix database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
